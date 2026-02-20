4247e13f0a2698b116630b437f2ac00c8435fdeb
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-21 01:49:02 +0600

    Initialized Home page

--- login.js
+++ login.js
@@ -1,1 +1,25 @@
-console.log("Ready to go")
+document.getElementById('login-btn').addEventListener('click', function(){
+
+    // Fetch mobile number
+
+    const numberInput = document.getElementById("input-number");
+
+    const userNumber = numberInput.value;
+
+    // Fetch pin number
+
+    const pinInput = document.getElementById('input-pin');
+
+    const userPin = pinInput.value;
+
+    // Check the validation by matching fetched data
+
+    if (userNumber === '01974243177' && userPin === '1234') {
+        alert('Login Completed');
+    }
+
+    else{
+        alert('Login Failed');
+        return;
+    }
+})
